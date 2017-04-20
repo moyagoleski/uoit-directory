ab0c632ae686df4eb3796cef447ad8cbdbaee117
Moya Goleski <moyagoleski@gmail.com>
2017-04-20 10:31:39 -0400

    Hides Search Result
    Hides the search result when you click another tab

--- uoit-directory/assets/js/app.js
+++ uoit-directory/assets/js/app.js
@@ -5,3 +5,7 @@ $("#searchResult").hide();
 $('#searchButton').click(function () {
   $("#searchResult").show();
 });
+
+$('.tabs').on("click", "li", function (event) {
+  $("#searchResult").hide();
+});

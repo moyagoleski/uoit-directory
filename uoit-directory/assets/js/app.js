3dccae5f8fb8382226d7532c29b807e8aaaaef71
Jason <Jason@Jasons-MacBook-Pro.local>
2017-04-21 11:37:02 -0400

    Revert "changed contact cards"
    # Conflicts:
    #	uoit-directory/index.html

--- uoit-directory/assets/js/app.js
+++ uoit-directory/assets/js/app.js
@@ -1,1 +1,11 @@
 $(document).foundation()
+
+$("#searchResult").hide();
+
+$('#searchButton').click(function () {
+  $("#searchResult").show();
+});
+
+$('.tabs').on("click", "li", function (event) {
+  $("#searchResult").hide();
+});

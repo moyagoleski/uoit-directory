fa326d70e7a04a7e22b8ff0ec800aa70a847abd5
Wose <Jacksonteather@hotmail.com>
2017-05-25 17:07:24 -0400

    Cleaned up gulpfile, added Babel + Browserify and templatecache tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,106 +1,98 @@
-/* File: gulpfile.js */
-
-// grab our gulp packages
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    sass = require('gulp-sass'),
-    rename = require('gulp-rename'),
-    minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
-    sourcemaps = require('gulp-sourcemaps'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    sassLint = require('gulp-sass-lint'),
-    notify = require('gulp-notify'),
-    templateCache = require('gulp-angular-templatecache'),
-    htmlmin = require('gulp-htmlmin'),
-    ngAnnotate = require('gulp-ng-annotate');
+	gutil = require('gulp-util'),
+	$ = require('gulp-load-plugins')(),
+	source = require('vinyl-source-stream'),
+	buffer = require('vinyl-buffer'),
+	browserify = require('browserify'),
+	watchify = require('watchify'),
+	babel = require('babelify');
 
+function bundle(watch) {
+	var bundler = watchify(browserify('./src/directory.js', {
+		debug: true
+	}).transform(babel, {
+		presets: ['es2015']
+	}));
 
+	function rebundle() {
+		bundler.bundle()
+			.on('error', function(err) { console.error(err); this.emit('end'); })
+			.pipe(source('build.js'))
+			.pipe(buffer())
+			.pipe($.sourcemaps.init({ loadMaps: true }))
+			.pipe($.ngAnnotate())
+			.pipe(gulp.dest('./dist/js'))
+			.pipe($.uglify())
+			.pipe($.rename({
+				suffix: '.min'
+			}))
+			.pipe($.sourcemaps.write('./'))
+			.pipe(gulp.dest('./dist/js'))
+			.pipe($.notify({
+				message: 'Scripts task complete'
+			}));
+	}
+
+	if (watch) {
+		bundler.on('update', function() {
+			console.log('-> bundling...');
+			rebundle();
+		});
+	}
+
+	rebundle();
+}
+
+function watchBundle() {
+	return bundle(true);
+};
+
+gulp.task('js-task', ['template-task'], function() {
+	return bundle();
+});
 
 gulp.task('scss-task', function() {
-    return gulp.src('src/scss/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .on('error', function(err) {
-            console.error('Error!', err.message);
-        })
-        .pipe(minifycss())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(sourcemaps.write())
-        .pipe(notify({
-            message: 'Styles task complete'
-        }));
+	return gulp.src('src/scss/**/*.scss')
+		.pipe($.sourcemaps.init())
+		.pipe($.sass())
+		.on('error', function(err) {
+			console.error('Error!', err.message);
+		})
+		.pipe($.minifyCss())
+		.pipe($.rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('dist/css'))
+		.pipe($.sourcemaps.write())
+		.pipe($.notify({
+			message: 'Styles task complete'
+		}));
 });
 
 gulp.task('html-task', function() {
-    // gulp.task('html-task', ['template'], function() {
-    return gulp.src('src/**/*.html')
-        .pipe(gulp.dest('dist'))
-        .pipe(notify({
-            message: 'html task complete'
-        }));
+	return gulp.src('src/**/*.html')
+		.pipe(gulp.dest('dist'))
+		.pipe($.notify({
+			message: 'html task complete'
+		}));
 });
 
-gulp.task('minify', function() {
-    return gulp.src('src/js/**/*.js')
-        .pipe(ngAnnotate())
-        .pipe(uglify())
-        .pipe(concat('all.min.js'))
-        .pipe(gulp.dest('dist/js/'))
-});
-
-
-// gulp.task('template', function() {
-//     return gulp.src('src/template/**/*.html')
-//         // .pipe(htmlmin({ collapseWhitespace: true }))
-//         .pipe(htmlmin({ collapseWhitespace: true, removeEmptyAttributes: true }))
-//         .pipe(templateCache({
-//             module: 'directorySearch',
-//             standalone: true
-//         }))
-//         .pipe(gulp.dest('dist/js'));
-// });
-
-// gulp.task('template', function() {
-//     return gulp.src('src/template/**/*.thml')
-//         .pipe(htmlmin({ removeEmptyAttributes: true }))
-//         .pipe(templateCache())
-//         .pipe(gulp.src('dist/js'));
-// });
-
-// gulp.task('minify', function() {  
-//     return gulp.src(['js/appService.js','js/app.js']) //注意，此处特意如此，避免顺序导致的问题
-//         .pipe(ngAnnotate())
-//         .pipe(ngmin({dynamic: false}))  
-//         .pipe(stripDebug())  
-//         .pipe(uglify({outSourceMap: false}))  
-//         .pipe(concat('all.min.js'))  
-//         .pipe(gulp.dest('js/'))  
-// });   
-
-gulp.task('js-task', function() {
-    return gulp.src('src/js/**/*.js')
-        .pipe(sourcemaps.init())
-        .pipe(gulp.dest('./dist/js'))
-        .pipe(sourcemaps.write('dist'))
-        .pipe(notify({
-            message: 'Scripts task complete'
-        }));
+gulp.task('template-task', function() {
+	return gulp.src('src/template/**/*.html')
+		.pipe($.angularTemplatecache('templates.run.js', {
+			templateHeader: `export const TemplateRun = ['$templateCache', function($templateCache) {`,
+			templateFooter: '}];'
+		}))
+		.pipe(gulp.dest('src/components'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('src/**/*.html', ['html-task']);
-    // gulp.watch('src/template/**/*.html', ['template']);
-    gulp.watch('src/scss/**/*.scss', ['scss-task']);
-    gulp.watch('src/js/**/*.js', ['minify']);
-
+	gulp.watch('src/**/*.html', ['html-task']);
+	gulp.watch('src/template/**/*.html', ['template-task']);
+	gulp.watch('src/scss/**/*.scss', ['scss-task']);
+	return watchBundle();
 });
 
-// 'template', 'scss', 'copyHtml', 'js'
-gulp.task('default', ['html-task', 'scss-task', 'minify', 'watch'], function() {
-    return gutil.log('Gulp is running!');
+gulp.task('default', ['html-task', 'scss-task', 'js-task', 'watch'], function() {
+	return gutil.log('Gulp is running!');
 });

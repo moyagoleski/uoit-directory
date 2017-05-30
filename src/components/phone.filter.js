34928c9330442714cac5849c1003d3c5cd03b7cf
Wose <Jacksonteather@hotmail.com>
2017-05-30 10:55:23 -0400

    Add filter for cleaning phone numbers into links

---
+++ src/components/phone.filter.js
@@ -0,0 +1,24 @@
+export const PhoneFilter = () => {
+  return tel => {
+    const maxLength = 10;
+    const cityCodeLength = 3;
+    const numberLength = 7;
+    // Return if no number was passed in
+    if (!tel) {
+      return '';
+    }
+    // Strip all non-numeric characters
+    let value = tel.toString().trim().replace(/\D/g, '');
+    // Trim to verify the model doesn't get any larger
+    if (value.length > maxLength) {
+      // If the first character is a country code
+      if (value.charAt(0) === '1') {
+        // Don't strip it, allow 11 digits
+        value = value.substring(1, maxLength + 1);
+      } else {
+        value = value.substring(0, maxLength);
+      }
+    }
+    return value;
+  }
+}
--- src/directory.js
+++ src/directory.js
@@ -4,6 +4,7 @@ import { ContactsConstant } from './components/contacts.constant';
 import { DirectoryService } from './components/users.service';
 import { SearchComponent } from './components/search.component';
 import { StartFromFilter } from './components/start-from.filter';
+import { PhoneFilter } from './components/phone.filter';
 import { TemplateRun } from './components/templates.run'
 
 angular.module('uoitDirectory', [])
@@ -11,6 +12,7 @@ angular.module('uoitDirectory', [])
 	.service('DirectoryService', DirectoryService)
 	.component('directorySearch', SearchComponent)
 	.filter('startFrom', StartFromFilter)
+	.filter('tel', PhoneFilter)
 	.run(TemplateRun);
 
 angular.bootstrap(document.body, ['uoitDirectory'], {

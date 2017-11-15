222f5dab82e99774d750cfc49b5bd8445dc0758d
Wose <Jacksonteather@hotmail.com>
2017-11-15 16:48:22 -0500

    🚿 Explicitly declare state inside central property (eliminates $scope)

--- src/components/directory-search.component.js
+++ src/components/directory-search.component.js
@@ -2,7 +2,6 @@ export const DirectorySearchComponent = {
   templateUrl: 'directory-search.component.html',
 	controller: class DirectorySearch {
 		constructor (
-			$scope,
 			$filter,
 			$http,
 			$element,
@@ -11,7 +10,6 @@ export const DirectorySearchComponent = {
 			DIRECTORY_CONTACTS
 		) {
 			'ngInject';
-			this.$scope = $scope;
 			this.$filter = $filter;
 			this.$http = $http;
 			this.$element = $element;
@@ -21,30 +19,36 @@ export const DirectorySearchComponent = {
 		}
 		
 		$onInit() {
-			this.$scope.contacts = this.DIRECTORY_CONTACTS;
-			this.$scope.departments = null;
-			this.$scope.users = null;
-			this.$scope.usersCache = null;
-			this.$scope.searchName = {};
+			this.$state = {
+				contacts: this.DIRECTORY_CONTACTS,
 
-			this.$scope.departmentError = null;
-			this.$scope.userError = null;
-			this.$scope.formStatus = {
-				success: null,
-				error: null
+				departments: null,
+				users: null,
+				usersCache: null,
+				searchName: {},
+
+				departmentError: null,
+				userError: null,
+				formStatus: {
+					success: null,
+					error: null
+				},
+				formData: {},
+
+				currentPage: 0,
+				pageSize: 7,
+				order: 'lastname',
+
+				loadingResults: false
 			};
-
-			this.$scope.currentPage = 0;
-			this.$scope.pageSize = 7;
-			this.$scope.loadingResults = false;
 
 			// Load departments on init
 			this.DirectoryService.getDepts()
 				.then(depts => {
-					this.$scope.departments = depts;
+					this.$state.departments = depts;
 				})
 				.catch(err => {
-					this.$scope.departmentError = err;
+					this.$state.departmentError = err;
 				});
 		}
 
@@ -56,59 +60,59 @@ export const DirectorySearchComponent = {
 		}
 
 		removeSearchResult() {
-			this.$scope.searchName = {};
+			this.$state.searchName = {};
 		}
 
 		// MODIFY SEARCH RESULT
 		// RESET CURRENT PAGE
 		// CLEAR DROPDOWN LIST
 		modifyResultResetDropdown() {
-			this.$scope.currentPage = 0;
-			this.$scope.searchName.department = '';
+			this.$state.currentPage = 0;
+			this.$state.searchName.department = '';
 			this.getSearchResults();
 		}
 
 		// RESET CURRENT PAGE
 		// CLEAR INPUT FIELD
 		modifyResultClearInput() {
-			this.$scope.currentPage = 0;
-			this.$scope.searchName.firstname = '';
-			this.$scope.searchName.lastname = '';
+			this.$state.currentPage = 0;
+			this.$state.searchName.firstname = '';
+			this.$state.searchName.lastname = '';
 			this.getSearchResults();
 		}
 
 		// PASS ORDERBY PARAMETER
 		// AND CHANGE ACTIVE FILTER BUTTON
 		sortBy(propertyName) {
-			this.$scope.order = propertyName;
+			this.$state.order = propertyName;
 		}
 
 		smoothScroll() {
-			const scrollTop = $element.find('#search-results').offset().top;
+			const scrollTop = this.$element.find('#search-results').offset().top;
 			$('html,body').animate({ scrollTop }, 'slow');
 		}
 
 		changePageAndScroll(plusOrMinus) {
-			this.$scope.currentPage = this.$scope.currentPage + plusOrMinus;
+			this.$state.currentPage = this.$state.currentPage + plusOrMinus;
 			this.smoothScroll()
 		}
 
 		gotoPageAndScroll(page) {
-			this.$scope.currentPage = page - 1;
+			this.$state.currentPage = page - 1;
 			this.smoothScroll()
 		}
 
 		// GET FILTERED DATA
 		getSearchResults() {
-			this.$scope.loadingResults = true;
-			if (this.$scope.usersCache) {
-				this.$scope.users = this.$filter('filter')(this.$scope.usersCache, this.$scope.searchName) || [];
-				this.$scope.loadingResults = false;
+			this.$state.loadingResults = true;
+			if (this.$state.usersCache) {
+				this.$state.users = this.$filter('filter')(this.$state.usersCache, this.$state.searchName) || [];
+				this.$state.loadingResults = false;
 			} else {
-				this.DirectoryService.getUsers(this.$scope.searchName)
+				this.DirectoryService.getUsers(this.$state.searchName)
 					.then(users => {
-						this.$scope.users = this.$filter('filter')(users, this.$scope.searchName) || [];
-						this.$scope.loadingResults = false;
+						this.$state.users = this.$filter('filter')(users, this.$state.searchName) || [];
+						this.$state.loadingResults = false;
 						// Proactively load the whole list in the background as soon
 						// as user makes their first query; early searches will be
 						// handled by the `getSearchResults()` return, which provides a
@@ -117,25 +121,25 @@ export const DirectorySearchComponent = {
 						return this.DirectoryService.getUsers()
 					})
 					.then(users => {
-						this.$scope.usersCache = users;
+						this.$state.usersCache = users;
 					})
 					.catch(err => {
-						this.$scope.userError = err;
-						this.$scope.loadingResults = false;
+						this.$state.userError = err;
+						this.$state.loadingResults = false;
 					})
 			}
 		}
 
 		// GET NUMBER OF PAGES
 		numberOfPages() {
-			return this.$scope.users && this.$scope.users.length ?
-				Math.ceil(this.$scope.users.length / this.$scope.pageSize) :
+			return this.$state.users && this.$state.users.length ?
+				Math.ceil(this.$state.users.length / this.$state.pageSize) :
 				0;
 		}
 
 		getPageNumbers() {
-			if (this.$scope.users && this.$scope.users.length) {
-				let numberOfPages = Math.ceil(this.$scope.users.length / this.$scope.pageSize);
+			if (this.$state.users && this.$state.users.length) {
+				let numberOfPages = Math.ceil(this.$state.users.length / this.$state.pageSize);
 				const pageNumArray = [];
 				while (numberOfPages) {
 					pageNumArray.push(numberOfPages);
@@ -149,7 +153,7 @@ export const DirectorySearchComponent = {
 
 		processForm() {
 			// pass in data as strings
-			const data = this.$httpParamSerializerJQLike(this.$scope.formData);
+			const data = this.$httpParamSerializerJQLike(this.$state.formData);
 			// set the headers so angular passing info as form data (not request payload)
 			const headers = {'Content-Type': 'application/x-www-form-urlencoded'};
 			// send http POST request
@@ -162,10 +166,10 @@ export const DirectorySearchComponent = {
 				.then(response => {
 					if (response.data.success) {
 						console.info(response);
-						this.$scope.formStatus.error = false;
-						this.$scope.formStatus.success = "Your update request was submitted successfully!";
-						this.$scope.updateForm.$setUntouched();
-						this.$scope.formData = {};
+						this.$state.formStatus.error = false;
+						this.$state.formStatus.success = "Your update request was submitted successfully!";
+						this.updateForm.$setUntouched();
+						this.$state.formData = {};
 					} else {
 						console.error(response);
 						throw new Error(response.data.message || 'An unknown error has occurred. Please try again!');
@@ -173,9 +177,9 @@ export const DirectorySearchComponent = {
 				})
 				.catch(response => {
 					console.error(response);
-					this.$scope.formStatus.success = false;
+					this.$state.formStatus.success = false;
 					const message = response.data.message || 'Please try again!';
-					this.$scope.formStatus.error = `There was an error submitting your update request. ${message}`;
+					this.$state.formStatus.error = `There was an error submitting your update request. ${message}`;
 				});
 		}
 	}

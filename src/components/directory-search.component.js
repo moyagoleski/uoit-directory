ac7bd29e7580c8e73a3d7599c55fb22c01184486
Wose <Jacksonteather@hotmail.com>
2017-11-15 16:03:58 -0500

    ⚡️ Refactor DirectorySearch controller into ES6 class

--- src/components/directory-search.component.js
+++ src/components/directory-search.component.js
@@ -1,142 +1,141 @@
 export const DirectorySearchComponent = {
   templateUrl: 'directory-search.component.html',
-  controller: function(
-		$scope,
-		$filter,
-		$http,
-		$element,
-		$httpParamSerializerJQLike,
-		DirectoryService,
-		DIRECTORY_CONTACTS
-	) {
-		'ngInject';
-		var ctrl = this; // ASSIGN `this` TO A VARIABLE FOR USE INSIDE FUNCTIONS
+	controller: class DirectorySearch {
+		constructor (
+			$scope,
+			$filter,
+			$http,
+			$element,
+			$httpParamSerializerJQLike,
+			DirectoryService,
+			DIRECTORY_CONTACTS
+		) {
+			'ngInject';
+			this.$scope = $scope;
+			this.$filter = $filter;
+			this.$http = $http;
+			this.$element = $element;
+			this.$httpParamSerializerJQLike = $httpParamSerializerJQLike;
+			this.DirectoryService = DirectoryService;
+			this.DIRECTORY_CONTACTS = DIRECTORY_CONTACTS;
+		}
+		
+		$onInit() {
+			this.$scope.contacts = this.DIRECTORY_CONTACTS;
+			this.$scope.departments = null;
+			this.$scope.users = null;
+			this.$scope.usersCache = null;
+			this.$scope.searchName = {};
 
-		ctrl.$onInit = function() {
-
-			$scope.contacts = DIRECTORY_CONTACTS;
-			$scope.departments = null;
-			$scope.users = null;
-			$scope.usersCache = null;
-			$scope.searchName = {};
-
-			$scope.departmentError = null;
-			$scope.userError = null;
-			$scope.formStatus = {
+			this.$scope.departmentError = null;
+			this.$scope.userError = null;
+			this.$scope.formStatus = {
 				success: null,
 				error: null
 			};
 
-			$scope.currentPage = 0;
-			$scope.pageSize = 7;
-			$scope.loadingResults = false;
+			this.$scope.currentPage = 0;
+			this.$scope.pageSize = 7;
+			this.$scope.loadingResults = false;
 
 			// Load departments on init
-			DirectoryService.getDepts()
-				.then(function(depts) {
-					$scope.departments = depts;
+			this.DirectoryService.getDepts()
+				.then(depts => {
+					this.$scope.departments = depts;
 				})
-				.catch(function(err) {
-					$scope.departmentError = err;
+				.catch(err => {
+					this.$scope.departmentError = err;
 				});
-
 		}
-		
-		ctrl.getUser = function(person) {
+
+		getUser(person) {
 			person.expert = {};
-			DirectoryService.getExpert(person).then(function(expert) {
+			this.DirectoryService.getExpert(person).then(expert => {
 				person.expert = expert || false;
-    	});
+			});
 		}
 
-		// MAKE `$scope` METHODS INTO CONTROLLER METHODS
-		// ASSIGN THEM TO OUR `ctrl` VARIABLE INSTEAD OF `$scope`, I.E. `this`
-		// USE THEM IN HTML WITH `ng-click="$ctrl.removeSearchResult()"`
-		// TRY TO COMPLETELY REMOVE `$scope` FROM CONTROLLER AND HTML
-		ctrl.removeSearchResult = function() {
-			$scope.searchName = {};
-		};
+		removeSearchResult() {
+			this.$scope.searchName = {};
+		}
 
 		// MODIFY SEARCH RESULT
 		// RESET CURRENT PAGE
 		// CLEAR DROPDOWN LIST
-		ctrl.modifyResultResetDropdown = function() {
-			$scope.currentPage = 0;
-			$scope.searchName.department = '';
-			ctrl.getSearchResults();
+		modifyResultResetDropdown() {
+			this.$scope.currentPage = 0;
+			this.$scope.searchName.department = '';
+			this.getSearchResults();
 		}
 
 		// RESET CURRENT PAGE
 		// CLEAR INPUT FIELD
-		ctrl.modifyResultClearInput = function() {
-			$scope.currentPage = 0;
-			$scope.searchName.firstname = '';
-			$scope.searchName.lastname = '';
-			ctrl.getSearchResults();
+		modifyResultClearInput() {
+			this.$scope.currentPage = 0;
+			this.$scope.searchName.firstname = '';
+			this.$scope.searchName.lastname = '';
+			this.getSearchResults();
 		}
 
 		// PASS ORDERBY PARAMETER
 		// AND CHANGE ACTIVE FILTER BUTTON
-		ctrl.sortBy = function(propertyName) {
-			$scope.order = propertyName;
-		};
-
-
-		ctrl.smoothScroll = function() {
-			const scrollTop = $element.find('#search-results').offset().top;
-			$('html,body').animate({ scrollTop }, 'slow');
-		};
-
-		// GIVE REUSABLE CHUNKS THEIR OWN METHODS
-		// TRY TO AVOID `ng-click="page = 1; fun = true; etc"`
-		ctrl.changePageAndScroll = function(plusOrMinus) {
-			$scope.currentPage = $scope.currentPage + plusOrMinus;
-			ctrl.smoothScroll()
+		sortBy(propertyName) {
+			this.$scope.order = propertyName;
 		}
 
-		ctrl.gotoPageAndScroll = function(page) {
-			$scope.currentPage = page - 1;
-			ctrl.smoothScroll()
+		smoothScroll() {
+			const scrollTop = $element.find('#search-results').offset().top;
+			$('html,body').animate({ scrollTop }, 'slow');
+		}
+
+		changePageAndScroll(plusOrMinus) {
+			this.$scope.currentPage = this.$scope.currentPage + plusOrMinus;
+			this.smoothScroll()
+		}
+
+		gotoPageAndScroll(page) {
+			this.$scope.currentPage = page - 1;
+			this.smoothScroll()
 		}
 
 		// GET FILTERED DATA
-		ctrl.getSearchResults = function() {
-			$scope.loadingResults = true;
-			if ($scope.usersCache) {
-				$scope.users = $filter('filter')($scope.usersCache, $scope.searchName) || [];
-				$scope.loadingResults = false;
+		getSearchResults() {
+			this.$scope.loadingResults = true;
+			if (this.$scope.usersCache) {
+				this.$scope.users = this.$filter('filter')(this.$scope.usersCache, this.$scope.searchName) || [];
+				this.$scope.loadingResults = false;
 			} else {
-				DirectoryService.getUsers($scope.searchName)
-					.then(function(users) {
-						$scope.users = $filter('filter')(users, $scope.searchName) || [];
-						$scope.loadingResults = false;
+				this.DirectoryService.getUsers(this.$scope.searchName)
+					.then(users => {
+						this.$scope.users = this.$filter('filter')(users, this.$scope.searchName) || [];
+						this.$scope.loadingResults = false;
 						// Proactively load the whole list in the background as soon
 						// as user makes their first query; early searches will be
 						// handled by the `getSearchResults()` return, which provides a
 						// quicker (albeit uncacheable) response and then immediately
 						// kicks off a new request for the cache.
-						return DirectoryService.getUsers()
+						return this.DirectoryService.getUsers()
 					})
-					.then(function(users) {
-						$scope.usersCache = users;
+					.then(users => {
+						this.$scope.usersCache = users;
 					})
-					.catch(function(err) {
-						$scope.userError = err;
-						$scope.loadingResults = false;
+					.catch(err => {
+						this.$scope.userError = err;
+						this.$scope.loadingResults = false;
 					})
 			}
-		};
+		}
 
-		// GET PAGES NUMBER
-		ctrl.numberOfPages = function() {
-			return $scope.users && $scope.users.length ?
-				Math.ceil($scope.users.length / $scope.pageSize) :
+		// GET NUMBER OF PAGES
+		numberOfPages() {
+			return this.$scope.users && this.$scope.users.length ?
+				Math.ceil(this.$scope.users.length / this.$scope.pageSize) :
 				0;
-		};
+		}
 
-		ctrl.getPageNumbers = function() {
-			if ($scope.users && $scope.users.length) {
-				let numberOfPages = Math.ceil($scope.users.length / $scope.pageSize);
+		getPageNumbers() {
+			if (this.$scope.users && this.$scope.users.length) {
+				let numberOfPages = Math.ceil(this.$scope.users.length / this.$scope.pageSize);
 				const pageNumArray = [];
 				while (numberOfPages) {
 					pageNumArray.push(numberOfPages);
@@ -146,38 +145,39 @@ export const DirectorySearchComponent = {
 			} else {
 				return [1];
 			}
-		};
+		}
 
-		// // process the form
-		$scope.processForm = function() {
-			$http({
-				method:  'POST',
-				url:     'mail.php',
-				data:    $httpParamSerializerJQLike($scope.formData), //$.param($scope.formData),  // pass in data as strings
-			  // paramSerializer: '$httpParamSerializerJQLike',
-				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded'
-				}  // set the headers so angular passing info as form data (not request payload)
+		processForm() {
+			// pass in data as strings
+			const data = this.$httpParamSerializerJQLike(this.$scope.formData);
+			// set the headers so angular passing info as form data (not request payload)
+			const headers = {'Content-Type': 'application/x-www-form-urlencoded'};
+			// send http POST request
+			this.$http({
+				method: 'POST',
+				url: 'mail.php',
+				headers,
+				data
 			})
-			.then(response => {
-				if (response.data.success) {
-					console.info(response);
-					$scope.formStatus.error = false;
-					$scope.formStatus.success = "Your update request was submitted successfully!";
-					$scope.updateForm.$setUntouched();
-					$scope.formData = {};
-				} else {
+				.then(response => {
+					if (response.data.success) {
+						console.info(response);
+						this.$scope.formStatus.error = false;
+						this.$scope.formStatus.success = "Your update request was submitted successfully!";
+						this.$scope.updateForm.$setUntouched();
+						this.$scope.formData = {};
+					} else {
+						console.error(response);
+						throw new Error(response.data.message || 'An unknown error has occurred. Please try again!');
+					}
+				})
+				.catch(response => {
 					console.error(response);
-					throw new Error(response.data.message || 'An unknown error has occurred. Please try again!');
-				}
-			})
-			.catch(response => {
-				console.error(response);
-				$scope.formStatus.success = false;
-				const message = response.data.message || 'Please try again!';
-				$scope.formStatus.error = `There was an error submitting your update request. ${message}`;
-			});
-		};
-
+					this.$scope.formStatus.success = false;
+					const message = response.data.message || 'Please try again!';
+					this.$scope.formStatus.error = `There was an error submitting your update request. ${message}`;
+				});
+		}
 	}
+	// end controller
 };
